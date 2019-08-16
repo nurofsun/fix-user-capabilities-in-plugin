c6c7bd8c51351a2b85bc4bb95ae5b6454008876a
noorofsun <nurrahmat2050@gmail.com>
2019-08-16 14:10:59 +0700

    adding header requirements more clearly

--- noor-frontend-delete.php
+++ noor-frontend-delete.php
@@ -1,68 +1,16 @@
 <?php
 /**
-* Plugin Name: WPorg Generate delete  Link
-*/
-//function wporg_generate_delete_link($content)
-//{
-//// run only for single post page
-//if (is_single() && in_the_loop() && is_main_query()) {
-//// add query arguments: action, post
-//$url = add_query_arg(
-//[
-//'action' => 'wporg_frontend_delete',
-//'post'   => get_the_ID(),
-//],
-//home_url()
-//);
-//return $content . ' <a href="' . esc_url($url) . '">' . esc_html__('Delete Post', 'wporg') . '</a>';
-//}
-//return null;
-//}
- 
-/**
-* request handler
-*/
-//function wporg_delete_post()
-//{
-//if (isset($_GET['action']) && $_GET['action'] === 'wporg_frontend_delete') {
- 
-//// verify we have a post id
-//$post_id = (isset($_GET['post'])) ? ($_GET['post']) : (null);
- 
-//// verify there is a post with such a number
-//$post = get_post((int)$post_id);
-//if (empty($post)) {
-//return;
-//}
- 
-//// delete the post
-//wp_trash_post($post_id);
- 
-//// redirect to admin page
-//$redirect = admin_url('edit.php');
-//wp_safe_redirect($redirect);
- 
-//// we are done
-//die;
-//}
-//}
-
-//function wporg_generate_delete_link_plugin()
-//{
-	//if (current_user_can('edit_others_posts')) {
-	/**
-	* add the delete link to the end of the post content
-	*/
-	//add_filter('the_content', 'wporg_generate_delete_link');
-	
-	/**
-	* register our request handler with the init hook
-	*/
-	//add_action('init', 'wporg_delete_post');
-	//}
-//}
-//add_action('plugins_loaded','wporg_generate_delete_link_plugin');
-function noor_generated_delete_link($content)
+ * Plugin Name: Noor Frontend Delete Link
+ * Version: 1.0
+ * Description: Adding Feature That Admin Can Delete Post Through Frontend
+ * Author: Noor
+ * Author URI: https://maxrooted.com
+ * License: GPL2
+ *
+ *
+ * @info fixed version current_user_can function undefined
+ */
+function noor_generate_delete_link($content)
 {
 	if ( is_single() && in_the_loop() && is_main_query() )
 	{
@@ -84,7 +32,7 @@ function noor_delete_post()
 {
 	if ( current_user_can('edit_others_posts') )
 	{
-		add_filter('the_content', 'noor_generated_delete_link');
+		add_filter('the_content', 'noor_generate_delete_link');
 
 		if ( isset($_GET['action']) && $_GET['action'] === 'noor_deleting_post' )
 		{
